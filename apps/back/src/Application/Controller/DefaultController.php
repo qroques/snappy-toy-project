93224a46e4e17263388b04747986bc87ec476867
Quentin Roques <quentin.roques@knplabs.com>
2023-03-10 11:42:05 +0100

    feat: add chrome php

--- apps/back/src/Application/Controller/DefaultController.php
+++ apps/back/src/Application/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Domain\Generator\PdfInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use HeadlessChromium\BrowserFactory;
 
 final class DefaultController extends AbstractController
 {
@@ -20,8 +21,32 @@ final class DefaultController extends AbstractController
 
     public function __invoke(string $requestedUrl = null): Response
     {
+
+        $browserFactory = new BrowserFactory();
+
+        // starts headless chrome
+        $browser = $browserFactory->createBrowser(['noSandbox' => true]);
+
+        try {
+            // creates a new page and navigate to an URL
+            $page = $browser->createPage();
+            $page->navigate($requestedUrl ?? $this->defaultUrl)->waitForNavigation();
+
+            // // get page title
+            // $pageTitle = $page->evaluate('document.title')->getReturnValue();
+
+            // // screenshot - Say "Cheese"! 😄
+            // $page->screenshot()->saveToFile('/foo/bar.png');
+
+            // pdf
+            $filename = tempnam(sys_get_temp_dir(), 'pdf');
+            $page->pdf(['printBackground' => false])->saveToFile($filename);
+        } finally {
+            // bye
+            $browser->close();
+        }
         return new BinaryFileResponse(
-            $this->pdf->generateFromUrl($requestedUrl ?? $this->defaultUrl)
+            $filename
         );
     }
 }
